d2479bd59d3024d6d09d1d7404d2551cf1ca88e7
Samiya Kazi <samiya.kazi09@gmail.com>
2024-02-26 01:52:43 +0600

    feat: added auth related navigation

--- client/src/Pages/Login.page.tsx
+++ client/src/Pages/Login.page.tsx
@@ -1,10 +1,33 @@
 import ReactFacebookLogin, { ReactFacebookFailureResponse, ReactFacebookLoginInfo } from "react-facebook-login";
 import config from "../config";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 function LoginPage() {
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const id = localStorage.getItem('fb-id');
+    console.log(id)
+    if (id) navigate('/camera');
+  }, [])
+
+  function validateLoginInfo (data: any) : data is ReactFacebookLoginInfo {
+    return  typeof data === 'object' && typeof data.name === 'string' && typeof data.id === 'string';
+  }
+
   const responseFacebook = (response: ReactFacebookLoginInfo | ReactFacebookFailureResponse) => {
-    console.log(response);
+    if (validateLoginInfo(response)) {
+      const successfulResponse = response as ReactFacebookLoginInfo;
+      const { name, id } = successfulResponse;
+
+      console.log(name, id);
+      localStorage.setItem('fb-id', id);
+      navigate('/camera');
+    } else {
+     console.log('Facebook login error:', response); 
+    }
   }
 
   return (
