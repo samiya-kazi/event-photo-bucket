f2c76526fc0ac3e5f08458ad29fed17e3ba38b09
Samiya Kazi <samiya.kazi09@gmail.com>
2024-02-26 00:37:39 +0600

    chore: removed unused imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css'
 import LoginPage from './Pages/Login.page';
